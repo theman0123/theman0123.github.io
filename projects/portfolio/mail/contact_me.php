be3abea867bffc0882b8a26a6c2b59f1c2c9cd68
spencer <spencajones@gmail.com>
2017-08-19 11:59:49 -0600

    commented out mail()

--- projects/portfolio/mail/contact_me.php
+++ projects/portfolio/mail/contact_me.php
@@ -9,7 +9,7 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
-exit()
+
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
@@ -21,6 +21,6 @@ $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
+//mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
